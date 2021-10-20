8cc87386cbd5e5c13243b41286eecc2852a4a423
codetective <iaminiodu@gmail.com>
2021-10-20 16:05:27 +0100

    created header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,12 @@
 import {
   Stack,
   Flex,
-  Button,
+  HStack,
   Box,
   Text,
   VStack,
   useBreakpointValue,
+  Center,
 } from "@chakra-ui/react";
 
 export default function Hero() {
@@ -25,11 +26,12 @@ export default function Hero() {
         px={useBreakpointValue({ base: 4, md: 8 })}
         bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
       >
-        <Stack maxW={"2xl"} align={"flex-start"} spacing={0}>
+        <Stack maxW={"2xl"} align={"flex-start"} spacing={5}>
           <Text
             color={"white"}
             align={["center", "right", "right"]}
             fontWeight={700}
+            lineHeight="1"
             fontFamily="Quicksand, -apple-system"
             fontSize={useBreakpointValue({
               base: "3xl",
@@ -56,8 +58,46 @@ export default function Hero() {
             with the aim of raising funds for the purpose of achieving our dream
             of owning a faculty bus within a few months.
           </Text>
+          <HStack spacing="5" w="full" justify={["center", "flex-end"]}>
+            <Center
+              w="84.11px"
+              h="79.15"
+              rounded="lg"
+              border="2px solid coral"
+              color="white"
+            >
+              <Stack p="3" spacing="0" align="center">
+                <Text
+                  fontSize="22px"
+                  fontWeight="bolder"
+                  fontFamily="Quicksand, -apple-system"
+                >
+                  ₦5M
+                </Text>
+                <Text>GOAL</Text>
+              </Stack>
+            </Center>
+            <Center
+              w="84.11px"
+              h="79.15"
+              rounded="lg"
+              border="2px solid coral"
+              color="white"
+            >
+              <Stack p="3" spacing="0" align="center">
+                <Text
+                  fontSize="22px"
+                  fontWeight="bolder"
+                  fontFamily="Quicksand, -apple-system"
+                >
+                  in 6
+                </Text>
+                <Text>MONTHS</Text>
+              </Stack>
+            </Center>
+          </HStack>
         </Stack>
-        <Box align="right" position="fixed" bottom="0" right="0" p="10">
+        <Box align="right" position="fixed" bottom="0" right="0" px="10" py="5">
           <Text
             textTransform="uppercase"
             color={"white"}
@@ -71,6 +111,18 @@ export default function Hero() {
             <Box as="span" color="coral" fontWeight="bold">
               {" "}
               #IMPACT
+            </Box>
+          </Text>
+        </Box>
+        <Box align="left" position="fixed" bottom="0" left="0">
+          <Text
+            textTransform="uppercase"
+            color={"white"}
+            fontWeight={300}
+            fontSize={useBreakpointValue({ base: "md", md: "md", lg: "md" })}
+          >
+            <Box fontSize="8px" as="span">
+              *image not representative of product
             </Box>
           </Text>
         </Box>
