b71bb42c0a757610950d95940c81d524c13525dc
codetective <iaminiodu@gmail.com>
2021-10-20 16:45:38 +0100

    created header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -72,7 +72,7 @@ export default function Hero() {
                   fontWeight="bolder"
                   fontFamily="Quicksand, -apple-system"
                 >
-                  WE:
+                  WE
                 </Text>
                 <Text>TARGET:</Text>
               </Stack>
